c1a16fe17a0fa24970cc486ef1ca1cbd13243de3
ISHIMIGNE <garethbale3113@gmail.com>
2021-09-10 16:45:34 +0900

    トリムと２真数表記に変更

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,60 @@
 use std::io;
 
 fn main() {
-    println!("サーバーのアドレスを入力してください");
+    eprint!("サーバーのアドレスを入力してください :");
     let mut server_address = String::new();
 
     io::stdin()
         .read_line(&mut server_address)
-        .expect("Failed to read line");
+        .expect("値が不正です");
 
-    println!("サーバーのサブネットマスクを入力してください");
+    eprint!("サーバーのサブネットマスクを入力してください :");
     let mut server_subnet = String::new();
 
     io::stdin()
         .read_line(&mut server_subnet)
-        .expect("サブネットが上手く入力されていません");
+        .expect("値が不正です");
 
-    println!("クライアントのアドレスを入力してください");
+    eprint!("クライアントのアドレスを入力してください :");
     let mut client_address = String::new();
 
     io::stdin()
         .read_line(&mut client_address)
-        .expect("アドレスが上手く入力されていません");
+        .expect("値が不正です");
 
-    println!("クライアントのサブネットマスクを入力してください");
+    eprint!("クライアントのサブネットマスクを入力してください :");
     let mut client_subnet = String::new();
 
     io::stdin()
         .read_line(&mut client_subnet)
-        .expect("サブネットが上手く入力されていません");
+        .expect("値が不正です");
 
-    let server_address: Vec<&str> = server_address.split('.').collect();
-    let server_subnet: Vec<&str> = server_subnet.split('.').collect();
-    let client_address: Vec<&str> = client_address.split('.').collect();
-    let client_subnet: Vec<&str> = client_subnet.split('.').collect();
-
-    let cnt = 0;
+    let server_address: Vec<&str> = server_address.trim().split('.').collect();
+    let server_subnet: Vec<&str> = server_subnet.trim().split('.').collect();
+    let client_address: Vec<&str> = client_address.trim().split('.').collect();
+    let client_subnet: Vec<&str> = client_subnet.trim().split('.').collect();
 
     for i in server_address.iter() {
-        i.parse::<i32>().unwrap();
+        let address = i.parse::<i32>().unwrap();
+        eprint!("{:0>8b}", address);
     }
     for j in server_subnet.iter() {
-        j.parse::<i32>().unwrap();
+        let address = j.parse::<i32>().unwrap();
+        eprint!("{:0>8b}", address);
     }
     for x in client_address.iter() {
-        x.parse::<i32>().unwrap();
+        let address = x.parse::<i32>().unwrap();
+        eprint!("{:0>8b}", address);
     }
     for y in client_subnet.iter() {
-        y.parse::<i32>().unwrap();
+        let address = y.parse::<i32>().unwrap();
+        eprint!("{:0>8b}", address);
     }
 }
+
+// fn calc_address(server_address: Vec<&str>) -> i32 {
+//     let address = server_address;
+//     for i in address.iter() {
+//         let readdress = i.parse::<i32>().unwrap()
+//     }
+// }
