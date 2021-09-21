089ba900a5221268bfd08fdad85ed9154934a560
ishimigne_note <garethbale3113@gmail.com>
2021-09-21 13:02:15 +0900

    完成 v1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 
 fn main() {
+    //標準入力から受け取り
     eprint!("サーバーのアドレスを入力してください : ");
     let mut server_address = String::new();
 
@@ -21,31 +22,55 @@ fn main() {
 
     io::stdin().read_line(&mut client_subnet).ok();
 
+    //入力されたアドレス、サブネットを標準出力
     eprint!("\nサーバーアドレス: {}", server_address);
     eprint!("サーバーサブネット: {}", server_subnet);
 
     eprint!("\nクライアントアドレス: {}", client_address);
     eprint!("クライアントサブネット: {}", client_subnet);
 
+    //ベクタ型（文字列）で保存
     let server_address: Vec<&str> = server_address.trim().split('.').collect();
     let server_subnet: Vec<&str> = server_subnet.trim().split('.').collect();
     let client_address: Vec<&str> = client_address.trim().split('.').collect();
     let client_subnet: Vec<&str> = client_subnet.trim().split('.').collect();
 
+    //上のベクタ型をクローン
+    let server_address_clone = server_address.clone();
+    let server_subnet_clone = server_subnet.clone();
+    let client_address_clone = client_address.clone();
+    let client_subnet_clone = client_subnet.clone();
+
+    //ベクタ型のドットを外し、数値に変換
     let result_server = calculate_net(server_address, server_subnet);
+    let connection_server_clone = calculate_net(client_address_clone, server_subnet_clone);
     let result_client = calculate_net(client_address, client_subnet);
+    let connection_client_clone = calculate_net(server_address_clone, client_subnet_clone);
 
-    println!("\nアドレス1 : {:0>8b}", result_server);
-    println!("アドレス2 : {:0>8b}", result_client);
-
+    //サーバ視点のネットワーク部を10進数表記に変換
     let result_server = shift_address(result_server);
-    let result_client = shift_address(result_client);
+    let result_server_clone = shift_address(connection_server_clone);
 
-    println!("{}", result_server.join("."));
-    println!("{}", result_client.join("."));
+    //クライアント視点のネットワーク部を10進数表記に変換
+    let result_client = shift_address(result_client);
+    let result_client_clone = shift_address(connection_client_clone);
+
+    //結果出力
+    println!("\nサーバー視点 : ");
+    eprint!("\t\tサーバNetAddr\t    : {}", result_server.join("."));
+    println!(
+        "\n\t\tクライアントNetAddr : {}",
+        result_server_clone.join(".")
+    );
+    println!("\nクライアント視点 : ");
+    eprint!("\t\tサーバNetAddr\t    : {}", result_client.join("."));
+    println!(
+        "\n\t\tクライアントNetAddr : {}\n",
+        result_client_clone.join(".")
+    );
 }
 
-//シフトして＆演算でネットワーク部を求める
+//シフトして＆演算でネットワーク部を求める && 接続可能か＆で求める
 fn calculate_net(addr: Vec<&str>, sub: Vec<&str>) -> i64 {
     let mut address: i64 = 0;
     let mut subnet: i64 = 0;
@@ -90,6 +115,3 @@ fn shift_address(net_add: i64) -> Vec<String> {
     }
     result_str
 }
-
-//接続可能か＆で求める
-fn connection_address() {}
