d833e2c4cb59fb7e03c0d98dc818ef8acb4e504b
ishimigne_note <garethbale3113@gmail.com>
2022-02-13 16:29:23 +0900

    完成v5

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,99 @@
 use std::io;
 
+///
+/// エントリーポイント
+///
 fn main() {
-    let (result_server, connection_server, result_client, connection_client) = input_cmd_line();
+    input_cmd_line();
+}
 
-    //結果出力
+///
+/// コマンドラインから入力された値を変数に格納
+///
+/// IPアドレスと、サブネットマスクを表示
+///
+fn input_cmd_line() {
+    let mut server_address = String::new();
+    let mut server_subnet = String::new();
+    let mut client_address = String::new();
+    let mut client_subnet = String::new();
+
+    eprint!("サーバーのアドレスを入力してください : ");
+    io::stdin()
+        .read_line(&mut server_address)
+        .expect("Please type a ip address!!!");
+
+    eprint!("サーバーのサブネットマスクを入力してください : ");
+    io::stdin()
+        .read_line(&mut server_subnet)
+        .expect("Please type a subnetmask!!!");
+
+    eprint!("クライアントのアドレスを入力してください : ");
+    io::stdin()
+        .read_line(&mut client_address)
+        .expect("Please type a ip address!!!");
+
+    eprint!("クライアントのサブネットマスクを入力してください : ");
+    io::stdin()
+        .read_line(&mut client_subnet)
+        .expect("Please type a subnetmask!!!");
+
+    //入力されたアドレス、サブネットを標準出力
+    eprint!("\nサーバーアドレス: {}", server_address);
+    eprint!("サーバーサブネット: {}", server_subnet);
+
+    eprint!("\nクライアントアドレス: {}", client_address);
+    eprint!("クライアントサブネット: {}", client_subnet);
+
+    type_conversion(server_address, server_subnet, client_address, client_subnet);
+}
+
+///
+/// 型をベクタ型に変換後、保存
+///
+/// calc_net()で数値に変換
+/// shift_address()で10進数表記に変換
+///
+fn type_conversion(
+    server_address: String,
+    server_subnet: String,
+    client_address: String,
+    client_subnet: String,
+) {
+    let server_address: Vec<&str> = server_address.trim().split('.').collect();
+    let server_subnet: Vec<&str> = server_subnet.trim().split('.').collect();
+    let client_address: Vec<&str> = client_address.trim().split('.').collect();
+    let client_subnet: Vec<&str> = client_subnet.trim().split('.').collect();
+
+    let result_server = calc_net(&server_address, &server_subnet);
+    let connection_server = calc_net(&client_address, &server_subnet);
+    let result_client = calc_net(&client_address, &client_subnet);
+    let connection_client = calc_net(&server_address, &client_subnet);
+
+    let result_server = shift_address(result_server);
+    let connection_server = shift_address(connection_server);
+    let result_client = shift_address(result_client);
+    let connection_client = shift_address(connection_client);
+
+    output_ping_check(
+        result_server,
+        connection_server,
+        result_client,
+        connection_client,
+    );
+}
+
+///
+/// 実行結果の出力
+///
+/// 接続可能かも出力
+///
+fn output_ping_check(
+    result_server: Vec<String>,
+    connection_server: Vec<String>,
+    result_client: Vec<String>,
+    connection_client: Vec<String>,
+) {
     println!("\nサーバー視点 : ");
     eprint!("\t\tサーバNetAddr\t    : {}", result_server.join("."));
     println!(
@@ -17,7 +107,6 @@ fn main() {
         connection_client.join(".")
     );
 
-    //接続可能か比較(文字列型のまま)
     if result_server == result_client && connection_server == connection_client {
         println!("接続可能！");
     } else {
@@ -25,59 +114,11 @@ fn main() {
     }
 }
 
-fn input_cmd_line() -> (Vec<String>, Vec<String>, Vec<String>, Vec<String>) {
-    eprint!("サーバーのアドレスを入力してください : ");
-    let mut server_address = String::new();
-    io::stdin().read_line(&mut server_address).ok();
-
-    eprint!("サーバーのサブネットマスクを入力してください : ");
-    let mut server_subnet = String::new();
-    io::stdin().read_line(&mut server_subnet).ok();
-
-    eprint!("クライアントのアドレスを入力してください : ");
-    let mut client_address = String::new();
-    io::stdin().read_line(&mut client_address).ok();
-
-    eprint!("クライアントのサブネットマスクを入力してください : ");
-    let mut client_subnet = String::new();
-    io::stdin().read_line(&mut client_subnet).ok();
-
-    //入力されたアドレス、サブネットを標準出力
-    eprint!("\nサーバーアドレス: {}", server_address);
-    eprint!("サーバーサブネット: {}", server_subnet);
-
-    eprint!("\nクライアントアドレス: {}", client_address);
-    eprint!("クライアントサブネット: {}", client_subnet);
-
-    //ベクタ型（文字列）で保存
-    let server_address: Vec<&str> = server_address.trim().split('.').collect();
-    let server_subnet: Vec<&str> = server_subnet.trim().split('.').collect();
-    let client_address: Vec<&str> = client_address.trim().split('.').collect();
-    let client_subnet: Vec<&str> = client_subnet.trim().split('.').collect();
-
-    //ベクタ型のドットを外し、数値に変換
-    let result_server = calc_net(&server_address, &server_subnet);
-    let connection_server = calc_net(&client_address, &server_subnet);
-    let result_client = calc_net(&client_address, &client_subnet);
-    let connection_client = calc_net(&server_address, &client_subnet);
-
-    //サーバ視点のネットワーク部を10進数表記に変換
-    let result_server = shift_address(result_server);
-    let connection_server = shift_address(connection_server);
-
-    //クライアント視点のネットワーク部を10進数表記に変換
-    let result_client = shift_address(result_client);
-    let connection_client = shift_address(connection_client);
-
-    (
-        result_server,
-        connection_server,
-        result_client,
-        connection_client,
-    )
-}
-
-//シフトして＆演算でネットワーク部を求める(所有権は移さず、参照)
+///
+/// シフトして＆演算でネットワーク部を求める
+///
+/// ※所有権は移さず、参照
+///
 fn calc_net(addr: &Vec<&str>, sub: &Vec<&str>) -> i64 {
     let mut address: i64 = 0;
     let mut subnet: i64 = 0;
@@ -107,7 +148,9 @@ fn calc_net(addr: &Vec<&str>, sub: &Vec<&str>) -> i64 {
     result
 }
 
-//ネットワーク部を10進数表記に変換
+///
+/// ネットワーク部を10進数表記に変換
+///
 fn shift_address(net_add: i64) -> Vec<String> {
     let mut i: i32 = 3;
     let mut result: i64;
